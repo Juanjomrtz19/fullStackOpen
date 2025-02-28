3adfbcee89a62e616ba7da1cdee3c3d233dbda94
juanjomrtz19 <juanjodev19@gmail.com>
2025-02-28 12:46:20 +0100

    exercise 2.9 done

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone:'040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
+  const [personsShown, setPersonsShown] = useState(persons);
 
   function handleName (event){
     setNewName(event.target.value);
+  }
+
+  function handleFilter(event){
+    setPersonsShown(persons.filter(person =>(person.name.includes(event.target.value))))
   }
 
   function handleForm(event){
@@ -20,11 +28,22 @@ const App = () => {
     }
 
     setPersons(persons.concat({name: newName, phone: newPhone}))
+    
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with 
+        <input
+          onChange={handleFilter}
+        />
+      </div>
+
+      <h2>Add a new</h2>
+
       <form onSubmit={handleForm}>
         <div>
           name: <input 
@@ -45,9 +64,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {
-        persons.map(person => (
+        personsShown.map(person => (
         <div key={person.name}> 
-          <p>{person.name} {person.phone}</p>
+          <p>{person.name} {person.number}</p>
         </div>
         ))
       }
